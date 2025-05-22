c2c8b3ec64d7a4abb78be2894d4c0191fd5116db
CyberkidAdithya <cyberadithya05@gmail.com>
2025-05-22 17:28:02 +0530

    testing

--- Augs/isles_no_aug_seg_cmix.py
+++ Augs/isles_no_aug_seg_cmix.py
@@ -18,7 +18,7 @@ OUTPUT_DIRECTORY = "./output/ISLESfolder_original_only"
 os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
 
 IMG_SIZE = 112
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 LEARNINGRATE = 0.0001
 EPOCHS = 100
 EARLYSTOPPING = 60
